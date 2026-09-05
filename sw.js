7839ee7b975c6af2bf80417b83eea0d449238c5b
sasan333333 <sasanrasoolzadeh.sr@gmail.com>
2026-09-05 08:21:42 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'trading-journal-v1';
+const ASSETS = ['./trading-journal.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((resp) => {
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
